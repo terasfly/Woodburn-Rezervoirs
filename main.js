721575eef2d37335b39b342e9f26e2a1c328afaa
terasfly <84103159+terasfly@users.noreply.github.com>
2023-03-16 12:12:10 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,48 @@
-const mainTitle = document.querySelectorAll('.drop-down__title')
-const subMenu = document.querySelectorAll('.drop-down-submenu')
+const mainTitle = document.querySelectorAll('.drop-down__title');
+const subMenu = document.querySelectorAll('.drop-down-submenu');
 
-mainTitle.forEach(mainBlockSub => {
-    mainBlockSub.addEventListener('mouseover', (e) => {
-        // Get the class name of the target element
-        const className = e.target.classList.value;
+mainTitle.forEach((mainBlockSub) => {
+  mainBlockSub.addEventListener('mouseover', (e) => {
+    // Get the class name of the target element
+    const className = e.target.classList.value;
 
-        // Hide all submenus
-        subMenu.forEach(subMenu => {
-            subMenu.style.display = 'none';
-        });
-
-        // Show the corresponding submenu
-        switch (className) {
-            case 'about':
-                document.querySelector('.sub-one').style.display = 'block';
-                break;
-            case 'activities':
-                document.querySelector('.sub-two').style.display = 'block';
-                break;
-            case 'lakes':
-                document.querySelector('.sub-three').style.display = 'block';
-                break;
-            case 'contacts':
-                document.querySelector('.sub-four').style.display = 'block';
-                break;
-            case 'location':
-                document.querySelector('.sub-five').style.display = 'block';
-                break;
-            default:
-                break;
-        }
+    // Hide all submenus
+    subMenu.forEach((subMenu) => {
+      subMenu.style.display = 'none';
     });
+
+    // Show the corresponding submenu
+    switch (className) {
+      case 'about':
+        document.querySelector('.sub-one').style.display = 'block';
+        break;
+      case 'activities':
+        document.querySelector('.sub-two').style.display = 'block';
+        break;
+      case 'lakes':
+        document.querySelector('.sub-three').style.display = 'block';
+        break;
+      case 'contacts':
+        document.querySelector('.sub-four').style.display = 'block';
+        break;
+      case 'location':
+        document.querySelector('.sub-five').style.display = 'block';
+        break;
+      default:
+        break;
+    }
+  });
+});
+
+subMenu.forEach((subMenu1) => {
+  subMenu1.addEventListener('mouseleave', () => {
+    subMenu1.style.display = 'none';
+  });
+});
+
+const burgerBtn = document.querySelector('.btn__burger');
+const burgerModal = document.querySelector('.btn__burger-dropList');
+
+burgerBtn.addEventListener('click', () => {
+  burgerModal.classList.toggle('active');
 });
